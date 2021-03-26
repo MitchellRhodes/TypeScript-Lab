4c95cb50002308d7d9879fafba31d6309927d8a9
Mitch Rhodes <M.Rhodes87@comcast.net>
2021-03-26 14:05:27 -0400

    finished lab

---
+++ typeScriptLab1.js
@@ -0,0 +1,89 @@
+"use strict";
+exports.__esModule = true;
+var mountainsArray = [
+    {
+        name: 'Kilimanjaro',
+        height: 19341
+    },
+    {
+        name: 'Everest',
+        height: 29029
+    },
+    {
+        name: 'Denali',
+        height: 20310
+    },
+];
+function findNameOfTallestMountain(mountains) {
+    var tallest = null;
+    for (var _i = 0, mountains_1 = mountains; _i < mountains_1.length; _i++) {
+        var mountain = mountains_1[_i];
+        if (!tallest) { //if tallest doesn't exist yet
+            tallest = mountain; //tallest is current mountain
+        }
+        if (mountain.height > tallest.height) {
+            tallest = mountain;
+        }
+    }
+    ;
+    return "" + (tallest === null || tallest === void 0 ? void 0 : tallest.name); //if tallest is null then the whole thing is null "elvis operator"
+}
+;
+console.log(findNameOfTallestMountain(mountainsArray));
+;
+var productArray = [
+    {
+        name: 'Bloodborne',
+        price: 19.99
+    },
+    {
+        name: 'Deathloop',
+        price: 53.99
+    },
+    {
+        name: 'Monster hunter rise',
+        price: 59.99
+    },
+    {
+        name: 'Resident Evil Village',
+        price: 59.99
+    }
+];
+function calcAverageProductPrice(products) {
+    var total = 0;
+    for (var _i = 0, products_1 = products; _i < products_1.length; _i++) {
+        var product = products_1[_i];
+        total += product.price;
+    }
+    var average = total / products.length;
+    return average;
+}
+;
+console.log(calcAverageProductPrice(productArray));
+;
+var inventoryArray = [
+    {
+        name: 'motor',
+        price: 10.00,
+        quantity: 10
+    },
+    {
+        name: 'sensor',
+        price: 12.50,
+        quantity: 4
+    },
+    {
+        name: 'LED',
+        price: 1.00,
+        quantity: 20
+    }
+];
+function calcInventoryValue(inventoryItems) {
+    var total = 0;
+    for (var _i = 0, inventoryItems_1 = inventoryItems; _i < inventoryItems_1.length; _i++) {
+        var item = inventoryItems_1[_i];
+        total += item.price * item.quantity;
+    }
+    return total;
+}
+console.log(calcInventoryValue(inventoryArray));
---
+++ typeScriptLab1.ts
@@ -0,0 +1,138 @@
+import { monitorEventLoopDelay } from "node:perf_hooks";
+
+interface Mountain {
+    name: string;
+    height: number;
+}
+
+
+
+
+const mountainsArray: Mountain[] = [
+    {
+        name: 'Kilimanjaro',
+        height: 19341,
+    },
+
+    {
+        name: 'Everest',
+        height: 29029,
+    },
+
+    {
+        name: 'Denali',
+        height: 20310,
+    },
+
+];
+
+
+
+
+function findNameOfTallestMountain(mountains: Mountain[]): string {
+    let tallest = null;
+
+    for (let mountain of mountains) {
+        if (!tallest) {//if tallest doesn't exist yet
+            tallest = mountain;//tallest is current mountain
+        }
+
+        if (mountain.height > tallest.height) {
+            tallest = mountain;
+        }
+    };
+    return `${tallest?.name}`; //if tallest is null then the whole thing is null "elvis operator"
+};
+
+console.log(findNameOfTallestMountain(mountainsArray));
+
+
+
+
+
+interface Product {
+    name: string;
+    price: number;
+};
+
+
+
+const productArray: Product[] = [
+    {
+        name: 'Bloodborne',
+        price: 19.99
+    },
+
+    {
+        name: 'Deathloop',
+        price: 53.99
+    },
+
+    {
+        name: 'Monster hunter rise',
+        price: 59.99
+    },
+
+    {
+        name: 'Resident Evil Village',
+        price: 59.99
+    }
+];
+
+
+function calcAverageProductPrice(products: Product[]): number {
+    let total = 0;
+
+    for (let product of products) {
+        total += product.price;
+    }
+
+    let average = total / products.length;
+    return average;
+};
+
+console.log(calcAverageProductPrice(productArray));
+
+
+
+
+
+
+
+interface Inventory extends Product {
+    quantity: number;
+};
+
+
+const inventoryArray: Inventory[] = [
+    {
+        name: 'motor',
+        price: 10.00,
+        quantity: 10
+    },
+
+    {
+        name: 'sensor',
+        price: 12.50,
+        quantity: 4
+    },
+
+    {
+        name: 'LED',
+        price: 1.00,
+        quantity: 20
+    }
+];
+
+
+function calcInventoryValue(inventoryItems: Inventory[]): number {
+    let total = 0;
+
+    for (let item of inventoryItems) {
+        total += item.price * item.quantity
+    }
+
+    return total;
+}
+
+console.log(calcInventoryValue(inventoryArray));
